3295206195f7af16189dcac3eaf367d5fa9de3a6
GbatZ2 <carloszaragozabeato15@gmail.com>
2022-11-20 10:05:51 +0100

    finished

---
+++ src/Components/BodyPage/AudioCard/AudioCard.js
@@ -0,0 +1,63 @@
+import React, { useContext, useEffect, useState} from "react";
+import { AudioContext } from "../../../Context/AudioContext";
+import notes from "../../../assets/images/notes.png"
+import download from "../../../assets/images/download.png"
+
+import './AudioCard.css'
+
+
+export default function AudioCard() {
+  const { audios,time,setValor,setTimer,timer,valor,setTime } = useContext(AudioContext);
+
+
+
+
+  useEffect(() => {
+  
+    setValor(Math.floor(Math.random() * audios.length))
+    setInterval(() => {
+      
+      setTimer((Math.floor(Math.random() * 3000000)+400000));
+      setTime(!time)
+    }, timer);
+
+
+    console.log(timer)
+
+  },[time])
+
+
+ 
+
+
+
+
+
+
+
+  
+
+  
+
+  
+
+
+
+
+
+  return (
+    <div className="cardAudio">
+      <figure className="figureNotes">
+        <img src={notes} alt="notes icon" />
+      </figure>
+
+      <h2 className="audioTitle">Audios</h2>
+        <audio id="audio" src={audios[valor]} autoPlay></audio>
+        <a href={audios[valor]} download={audios[valor]} className="download">
+        <figure className="figureDownload">
+          <img src={download} alt="download button" />
+        </figure>
+      </a>
+    </div>
+  );
+}
